348a1000528b74b72f2d9f494f2ce23e9a41d339
INTRA\asela.illayapparachc <asela.illayapparachc@rakuten.com>
2018-10-31 18:09:36 +0800

    changes

---
+++ BasicProject/basic.py
@@ -0,0 +1,13 @@
+"""
+ This is a simple python script
+"""
+
+def myfunc():
+    """
+     My Function
+    """
+    first = 1
+    second = 1
+    print(f'{second} and {first}')
+
+myfunc()
---
+++ BasicProject/cap.py
@@ -0,0 +1,9 @@
+
+def cap_text(text):
+    ''' 
+    capitalize text
+    '''
+    return text.capitalize()
+
+if __name__ == "__main__":
+    print(cap_text("hello, world"))
